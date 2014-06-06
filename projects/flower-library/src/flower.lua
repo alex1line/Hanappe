ec155a46bfc708c8eb542f61506f271a911c2992
alex1line <alex1line@gmail.com>
2014-06-06 23:55:41 +0400

    + getCurrentSceneName + getPrevSceneName

--- projects/flower-library/src/flower.lua
+++ projects/flower-library/src/flower.lua
@@ -205,6 +205,14 @@ function M.callOnce(func, ...)
     return Executors.callOnce(func, ...)
 end
 
+function M.getCurrentSceneName()
+    return SceneMgr:getCurrentSceneName()
+end
+
+function M.getPrevSceneName()
+    return SceneMgr:getPrevSceneName()
+end
+
 ----------------------------------------------------------------------------------------------------
 -- @type class
 --
@@ -1309,6 +1317,7 @@ M.SceneMgr = SceneMgr
 SceneMgr.scenes = {}
 SceneMgr.currentScene = nil
 SceneMgr.nextScene = nil
+SceneMgr.prevScene = nil
 SceneMgr.nextSceneIndex = nil
 SceneMgr.closingSceneSize = nil
 SceneMgr.closingSceneGroup = nil
@@ -1453,6 +1462,7 @@ function SceneMgr:closeScene(params)
 
         self.closingSceneGroup = nil
         self.closingSceneSize = nil
+        self.prevScene = self.currentScene
         self.currentScene = self.nextScene
         self.nextScene = nil
         self.transitioning = false
@@ -1565,6 +1575,16 @@ function SceneMgr:onEnterFrame(e)
             scene:dispatchEvent(Event.UPDATE)
         end
     end
+end
+
+function SceneMgr:getCurrentSceneName()
+    local scene = self.currentScene
+    return scene and scene.name or ""
+end
+
+function SceneMgr:getPrevSceneName()
+    local scene = self.prevScene
+    return scene and scene.name or ""
 end
 
 ----------------------------------------------------------------------------------------------------
